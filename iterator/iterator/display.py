472713f1ea4eaeba53c451ba633dafb49dcb19aa
adhikari011 <143756597+adhikari011@users.noreply.github.com>
2024-02-19 12:39:01 -0500

    Update display.py

--- iterator/iterator/display.py
+++ iterator/iterator/display.py
@@ -5,14 +5,16 @@ from typing import List
 
 def convert_bool_to_answer(argument: bool):
     """Return a string-based and human-readable representation of a bool."""
-    # TODO: Return a string-based and human-readable version of a bool
-    # TODO: For instance, if argument is False then return "No"
-    # TODO: For instance, if argument is True then return "Yes"
+    # For instance, if argument is False then return "No"
+    if argument is False:
+        return "No"
+    # For instance, if argument is True then return "Yes"
+    return "Yes"
 
 
 def display_list(values: list, indent=""):
     """Display the provided list when iterating and printing every indented value."""
-    # TODO: Iterate through all of the values inside of the list,
+    # Iterate through all of the values inside of the list,
     # displaying them in the following fashion as shown in the README.md for this project
-    # TODO: ensure that the output is indented by the numbers of spaces in the indent
-    # TODO: the display should be of the format: {indent}2**{index} = {value}
+    for index, value in enumerate(values):
+        print(f"{indent}2**{index} = {value}")
